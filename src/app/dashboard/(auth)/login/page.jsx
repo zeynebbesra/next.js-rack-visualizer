d2d2fef0ddcdd2bad086627e3ce02dc9d04fd3eb
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-09-01 14:21:33 +0300

    login page created

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,11 +4,33 @@ import styles from './page.module.css'
 import { signIn } from "next-auth/react"
 
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const email = e.target[0].value
+    const password = e.target[1].value
+
+    signIn("credentials", {email, password})
+  }
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
       <button onClick={()=> signIn("google")}>Login with Google</button>
     </div>
   )
 }
-
+ 
 export default Login
