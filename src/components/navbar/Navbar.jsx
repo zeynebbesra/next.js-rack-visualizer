3f09d10b1a010e2419ea6885ecb3bda27bb9be89
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-09-01 18:57:54 +0300

    usesession function used for logout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import React from 'react'
 import styles from './navbar.module.css'
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
     {
@@ -34,6 +35,7 @@ const links = [
 ]
 
 const Navbar = () => {
+    const session = useSession();
   return (
     <div className={styles.container}>
         <Link href="/" className={styles.logo}>SIEMENS</Link>
@@ -43,15 +45,11 @@ const Navbar = () => {
                     {link.title}
                 </Link>
             ))}
-
-            <button
-                className={styles.logout}
-                onClick={() => {
-                    console.log("Logged out")
-                }}
-            >
-                Logout
-            </button>
+            {session.status === "authenticated" && (
+                <button className={styles.logout} onClick={signOut}>
+                    Logout
+                </button>
+            )}
         </div>
     </div>
   )
